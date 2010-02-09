5095c5c68f7752b6d4813872fe033dd654d7a849
chatgris <chatgri@gmail.com>
2010-02-09 20:23:58 +0100

    Pretty urls for will_paginate

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :sessions
   map.resources :users
+  map.connect '/topics/page/:page', :controller => 'topics'
+  map.connect '/topics/:id/page/:page', :controller => 'topics', :action => 'show'
   map.resources :topics, :has_many => :posts
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
