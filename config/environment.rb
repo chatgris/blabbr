f60ab85e3d724252fb1123d9a3d58f9eddd25de6
chatgris <chatgri@gmail.com>
2010-02-13 16:03:12 +0100

    Custom i18n and will_paginate

--- config/environment.rb
+++ config/environment.rb
@@ -7,9 +7,8 @@ Rails::Initializer.run do |config|
   config.gem 'mongo_mapper'
   config.gem 'mongo_ext', :lib => false
   config.gem 'mongo'
-  config.gem 'agnostic-will_paginate', :version => '3.0.0', :lib => 'will_paginate'
+  config.gem 'will_paginate', :version => '2.3.12', :lib => 'will_paginate'
   config.gem 'RedCloth'
-  config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'config', 'locales', 'blabber', '*.{rb,yml}')]
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
   config.frameworks -= [:active_record]
   config.time_zone = 'UTC'
