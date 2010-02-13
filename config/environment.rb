ec05301677c8d426d4e98ffccf5ea2c663bd72ea
chatgris <chatgri@gmail.com>
2010-02-13 18:51:41 +0100

    Specify mongo gem version

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
   config.gem 'haml'
-  config.gem 'mongo_mapper'
-  config.gem 'mongo_ext', :lib => false
-  config.gem 'mongo'
+  config.gem 'mongo_mapper', :version => '0.6.10'
+  config.gem 'mongo_ext', :lib => false, :version => '0.18.2'
+  config.gem 'mongo', :version => '0.18.2'
   config.gem 'will_paginate', :version => '2.3.12', :lib => 'will_paginate'
   config.gem 'RedCloth'
   config.i18n.load_path += Dir[File.join(RAILS_ROOT, 'config', 'locales', '**', '*.{rb,yml}')]
