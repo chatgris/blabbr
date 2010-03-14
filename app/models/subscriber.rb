fb8a59f87d8717548b6587ec1c818ed4bdad465e
chatgris <chatgri@gmail.com>
2010-03-14 20:12:48 +0100

    Default value of unread posts

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -2,6 +2,6 @@ class Subscriber
   include Mongoid::Document
 
   field :nickname, :type => String
-  field :message, :type => Integer, :default => 0
+  field :message, :type => Integer, :default => 1
   
 end
