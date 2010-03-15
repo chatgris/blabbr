5ea88ab4026e7ae3a6d66673284ff10a2ad1fd6b
chatgris <chatgri@gmail.com>
2010-03-15 06:49:52 -0700

    Sass style set to compressed

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Blabbr::Application.initialize!
-Sass::Plugin.options[:style] = :compact
+Sass::Plugin.options[:style] = :compressed
 Sass::Plugin.options[:template_location] = File.join(Rails.root, "app", "stylesheets")
