39f92aad2c425444ff384fdfed4e70a6d308f223
chatgris <chatgri@gmail.com>
2010-04-28 07:19:20 +0200

    Mongoid configuration

--- config/application.rb
+++ config/application.rb
@@ -5,8 +5,7 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-require "rails/test_unit/railtie"
-require 'will_paginate'
+require "mongoid/railtie"
 
 # Auto-require default libraries and those for the current Rails environment.
 Bundler.require :default, Rails.env
