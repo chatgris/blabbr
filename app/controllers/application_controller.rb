d5e5cd2795418852f22a4f847aa53373367af783
chatgris <chatgri@gmail.com>
2010-05-01 18:11:47 +0200

    Some code cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,47 +1,4 @@
 class ApplicationController < ActionController::Base
-  helper :all
-  helper_method :current_user, :logged_user?, :mobile_device?
   protect_from_forgery
-  before_filter :instantiate_controller_and_action_names, :current_user, :set_locale, :prepare_for_mobile
-  
-  protected
- 
-  def current_user
-    @current_user = User.find(session[:current_user]) if session[:current_user]
-  end
-  
-  def logged_user?
-    session[:current_user]
-  end
-  
-  def redirect_if_no_logged
-    unless logged_user?
-      flash[:error] = "You're not authorised to view this page"
-      redirect_to login_path
-    end
-  end
-  
-  def instantiate_controller_and_action_names
-    @current_action = action_name
-    @current_controller = controller_name
-  end
-  
-  def set_locale 
-    I18n.locale = @current_user.locale if @current_user
-  end 
-  
-  def mobile_device?
-    if session[:mobile_param]
-      session[:mobile_param] == "1"
-    else
-      request.user_agent =~ /Mobile|webOS/
-    end
-  end
-
-  def prepare_for_mobile
-    session[:mobile_param] = params[:mobile] if params[:mobile]
-    request.format = :mobile if mobile_device?
-  end
 
 end
-
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,58 +5,13 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { "Blabber - #{page_title}" }
   end
-  
-  def format_text(text)
-    smilies(RedCloth.new(text.to_s).to_html(:textile))
-  end
-  
+
   def stylesheet(*args)
     content_for(:head) { stylesheet_link_tag(*args) }
   end
-  
+
   def javascript(*args)
     content_for(:head) { javascript_include_tag(*args) }
   end
-  
-  def unread_posts(subscribers)
-    subscribers.each do |s|
-      return s.message if s.nickname == @current_user.nickname
-    end
-  end
-  
-  def available_i18n
-    dirs = Dir.entries("#{Rails.root}/config/locales/")
-    
-    locales = []
-    dirs.each do |dir|
-      if File.extname(dir) == '.yml' && dir != '.' && dir != '..'
-        locales << dir.gsub(File.extname(dir), '')
-      end
-    end
-    locales
-  end
-  
-  def gravatar_url(email,gravatar_options={})
-    gravatar_options[:size] ||= nil 
-    gravatar_options[:default] ||= nil
-    grav_url = 'http://www.gravatar.com/avatar.php?'
-    grav_url << "gravatar_id=#{Digest::MD5.new.update(email)}" 
-    grav_url << "&size=#{gravatar_options[:size]}" if gravatar_options[:size]
-    grav_url << "&default=#{gravatar_options[:default]}" if gravatar_options[:default]
-    grav_url
-  end
-  
-  def smilies(text)
-    dirs = Dir.entries("#{Rails.root}/public/images/smilies")
-
-    dirs.each do |dir|
-      unless dir == '.' || dir == '..'
-        dir_name = dir.gsub(File.extname(dir), '')
-        text.gsub!(/:#{dir_name}/, "<img src=\"/images/smilies/#{dir}\" alt=\"#{dir_name}\" />")
-      end
-    end
-    text
-  end
-
 
 end
