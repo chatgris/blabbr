f5194455c00329582a89f30ac0f03a52e4f46341
chatgris <chatgri@gmail.com>
2010-05-04 14:08:09 +0200

    Resizing avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  process :resize_to_fit => [80, 160]
 
   def store_dir
     'images/avatars'
@@ -7,7 +10,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-
 
   def filename
     model.permalink + File.extname(super.to_s)
