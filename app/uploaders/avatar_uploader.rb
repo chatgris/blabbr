6a3ead7703ed929d1db68ddcc0a31ee57f1075d3
chatgris <chatgri@gmail.com>
2010-05-04 21:06:07 +0200

    Carrierwave, making cache_dir location heroku compliant

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -7,6 +7,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
     'images/avatars'
   end
 
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
