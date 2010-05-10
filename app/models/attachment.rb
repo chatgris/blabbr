7a89dd1d39af165202b9d38f19a571d464a3cc16
chatgris <chatgri@gmail.com>
2010-05-10 13:20:30 +0200

    Attachment polymorphic

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -4,12 +4,11 @@ class Attachment
   include Mongoid::Document
 
   field :nickname, :type => String
-  field :avatar, :type => String
   field :attachment, :type => String
 
   mount_uploader :attachment, AttachmentUploader
 
-  embedded_in :topic, :inverse_of => :subscribers
+  embedded_in :attachmentable, :inverse_of => :attachments
 
   validates_presence_of :nickname
 
