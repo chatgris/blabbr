0c1d60f11b64db1ff9e5216aee947187e12bd7a2
chatgris <chatgri@gmail.com>
2010-05-10 13:26:08 +0200

    Add some new fields to Subscriber

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,8 +1,10 @@
 class Subscriber
   include Mongoid::Document
 
-  field :nickname, :type => String
+  field :nickname
   field :unread, :type => Integer, :default => 0
+  field :page, :type => Integer
+  field :post_id
 
   embedded_in :topic, :inverse_of => :subscribers
 
--- spec/models/subscriber_spec.rb
+++ spec/models/subscriber_spec.rb
@@ -4,6 +4,12 @@ describe Subscriber do
 
   it { Subscriber.fields.keys.should be_include('nickname')}
   it { Subscriber.fields['nickname'].type.should == String}
+  it { Subscriber.fields.keys.should be_include('unread')}
+  it { Subscriber.fields['unread'].type.should == Integer}
+  it { Subscriber.fields.keys.should be_include('page')}
+  it { Subscriber.fields['page'].type.should == Integer}
+  it { Subscriber.fields.keys.should be_include('post_id')}
+  it { Subscriber.fields['post_id'].type.should == String}
 
   describe 'validation' do
     it 'should required title' do
