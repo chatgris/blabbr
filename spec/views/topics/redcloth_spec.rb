404392c2e0cdea0695a2855ddbe8a47147afbf4d
chatgris <chatgri@gmail.com>
2010-05-19 20:06:01 +0200

    textilize spec

--- spec/views/topics/redcloth_spec.rb
+++ spec/views/topics/redcloth_spec.rb
@@ -1,38 +1,41 @@
 require 'spec_helper'
 
 describe Topic do
-  def textilize(text)
-    Textilizer.new(text).to_html
-  end
+  describe "textilize" do
 
-  before do
-    @smiley = Factory.build(:smiley)
-    @smiley.image = File.open(Rails.root.join("image.jpg"))
-    @smiley.save
-  end
+    def textilize(text)
+      Textilizer.new(text).to_html
+    end
 
-  it "should do basic textile" do
-    textilize("hello *world*").should == "<p>hello <strong>world</strong></p>"
-  end
+    before do
+      @smiley = Factory.build(:smiley)
+      @smiley.image = File.open(Rails.root.join("image.jpg"))
+      @smiley.save
+    end
 
-  it "should allow img tag" do
-    textilize("!http://www.example.com/image.jpg!").should == "<p><img src=\"http://www.example.com/image.jpg\" /></p>"
-  end
+    it "should do basic textile" do
+      textilize("hello *world*").should == "<p>hello <strong>world</strong></p>"
+    end
 
-  it "should allow link on images" do
-    textilize("!http://www.example.com/image.jpg!:http://example.com/link").should == "<p><a href=\"http://example.com/link\"><img src=\"http://www.example.com/image.jpg\" /></a></p>"
-  end
+    it "should allow img tag" do
+      textilize("!http://www.example.com/image.jpg!").should == "<p><img src=\"http://www.example.com/image.jpg\" /></p>"
+    end
 
-  it "should allow link tag" do
-    textilize("\"link\":http://redcloth.org").should == "<p><a href=\"http://redcloth.org\">link</a></p>"
-  end
+    it "should allow link on images" do
+      textilize("!http://www.example.com/image.jpg!:http://example.com/link").should == "<p><a href=\"http://example.com/link\"><img src=\"http://www.example.com/image.jpg\" /></a></p>"
+    end
 
-  it "should escape javascript" do
-    textilize("<script type=\"text/javascript\">alert(\"test\");</script>").should == "alert(\"test\");"
-  end
+    it "should allow link tag" do
+      textilize("\"link\":http://redcloth.org").should == "<p><a href=\"http://redcloth.org\">link</a></p>"
+    end
 
-  it "should allow smilies" do
-    textilize("Test de smiley :doc:").should == "<p>Test de smiley <img src=\"/uploads/smilies/doc.jpg\" alt=\"doc\" /></p>"
+    it "should escape javascript" do
+      textilize("<script type=\"text/javascript\">alert(\"test\");</script>").should == "alert(\"test\");"
+    end
+
+    it "should allow smilies" do
+      textilize("Test de smiley :doc:").should == "<p>Test de smiley <img src=\"/uploads/smilies/doc.jpg\" alt=\"doc\" /></p>"
+    end
   end
 
 end
