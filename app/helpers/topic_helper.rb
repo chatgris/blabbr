a4a5183942b1742e9a91a0962dbd46c520ff4322
chatgris <chatgri@gmail.com>
2010-05-27 22:09:23 +0200

    post_ratio helper method

--- app/helpers/topic_helper.rb
+++ app/helpers/topic_helper.rb
@@ -2,4 +2,10 @@ module TopicHelper
   def textilize(text)
     Textilizer.new(text).to_html unless text.blank?
   end
+
+  def post_ratio(topic)
+    topic.members.each do |member|
+      return "#{member.posts_count}/#{topic.posts_count}" if member.nickname == current_user.nickname
+    end
+  end
 end
