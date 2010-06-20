f2268c2ff6863dda2f12ab425ace3e266f7ab9d3
chatgris <chatgri@gmail.com>
2010-06-20 14:12:25 +0200

    Carrierwave grid config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,11 @@
 CarrierWave.configure do |config|
   config.grid_fs_database = Mongoid.database.name
-  config.grid_fs_host = Mongoid.database.connection.host
-  config.grid_fs_access_url = "/uploads"
+  config.grid_fs_host = Mongoid.config.master.connection.host
+  config.grid_fs_port = Mongoid.config.master.connection.port
+  unless Mongoid.config.master.connection.auths.empty?
+    config.grid_fs_username = Mongoid.config.master.connection.auths[0]["username"]
+    config.grid_fs_password = Mongoid.config.master.connection.auths[0]["password"]
+  end
   config.storage = :grid_fs
+  config.grid_fs_access_url = "/uploads"
 end
