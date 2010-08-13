9d13255bcab2d4035e4292b60fe0b0614ff57f96
chatgris <chatgri@gmail.com>
2010-08-13 17:11:25 +0200

    Refactoring User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,70 +6,83 @@ describe User do
     @user = Factory.build(:user)
   end
 
-  it { User.fields.keys.should be_include('nickname')}
-  it { User.fields['nickname'].type.should == String}
+  describe "Fields" do
+    it { User.fields.keys.should be_include('nickname')}
+    it { User.fields['nickname'].type.should == String}
 
-  it { User.fields.keys.should be_include('email')}
-  it { User.fields['email'].type.should == String}
+    it { User.fields.keys.should be_include('email')}
+    it { User.fields['email'].type.should == String}
 
-  it { User.fields.keys.should be_include('permalink')}
-  it { User.fields['permalink'].type.should == String}
+    it { User.fields.keys.should be_include('permalink')}
+    it { User.fields['permalink'].type.should == String}
 
-  it { User.fields.keys.should be_include('locale')}
-  it { User.fields['locale'].type.should == String}
+    it { User.fields.keys.should be_include('locale')}
+    it { User.fields['locale'].type.should == String}
 
-  it { User.fields.keys.should be_include('note')}
-  it { User.fields['note'].type.should == String}
+    it { User.fields.keys.should be_include('note')}
+    it { User.fields['note'].type.should == String}
 
-  it { User.fields.keys.should be_include('posts_count')}
-  it { User.fields['posts_count'].type.should == Integer}
+    it { User.fields.keys.should be_include('posts_count')}
+    it { User.fields['posts_count'].type.should == Integer}
 
-  it { User.fields.keys.should be_include('gravatar_url')}
-  it { User.fields['gravatar_url'].type.should == String}
+    it { User.fields.keys.should be_include('gravatar_url')}
+    it { User.fields['gravatar_url'].type.should == String}
 
-  it { User.fields.keys.should be_include('attachments_count')}
-  it { User.fields['attachments_count'].type.should == Integer}
-
-  it "should be valid" do
-    @user.should be_valid
+    it { User.fields.keys.should be_include('attachments_count')}
+    it { User.fields['attachments_count'].type.should == Integer}
   end
 
   describe 'validation' do
 
-    before :each do
-      Factory.create(:user)
+    describe "Factory should be valid" do
+      it "should be valid" do
+        @user.should be_valid
+      end
     end
 
-    it 'should required nickname' do
-      Factory.build(:user, :nickname => '').should_not be_valid
+    describe "Uniqueness and presence validations" do
+
+      before :each do
+        Factory.create(:user)
+      end
+
+      it 'should required nickname' do
+        Factory.build(:user, :nickname => '').should_not be_valid
+      end
+
+      it 'should required permalink' do
+        Factory.build(:user, :permalink => '', :nickname => '').should_not be_valid
+      end
+
+      it 'should required email' do
+        Factory.build(:user, :email => '').should_not be_valid
+      end
+
+      it 'should not valid a nickname.size > 40' do
+        Factory.build(:user, :nickname => "511843606ed9fcb0d1005a7b7b4e9598b07eae20-ea502f0c5590a49d7d429d880edfbc487fe99053").should_not be_valid
+      end
+
+      it 'should not valid if login is already taken' do
+        Factory.build(:user, :nickname => 'one_user').should_not be_valid
+      end
+
+      it 'should not valid if email is already taken' do
+        Factory.build(:user, :email => 'email@mail.com').should_not be_valid
+      end
     end
 
-    it 'should required permalink' do
-      Factory.build(:user, :permalink => '', :nickname => '').should_not be_valid
-    end
+    describe "Callbacks validations" do
+      before :each do
+        Factory.create(:user)
+      end
 
-    it 'should required email' do
-      Factory.build(:user, :email => '').should_not be_valid
-    end
+      it "should have a valid permalink" do
+        User.by_permalink(@user.permalink).first.permalink.should == @user.nickname.parameterize
+      end
 
-    it 'should not valid a nickname.size > 40' do
-      Factory.build(:user, :nickname => "511843606ed9fcb0d1005a7b7b4e9598b07eae20-ea502f0c5590a49d7d429d880edfbc487fe99053").should_not be_valid
-    end
-
-    it 'should not valid if login is already taken' do
-      Factory.build(:user, :nickname => 'one_user').should_not be_valid
-    end
-
-    it 'should not valid if email is already taken' do
-      Factory.build(:user, :email => 'email@mail.com').should_not be_valid
-    end
-
-    it "should have a valid permalink" do
-      User.by_permalink(@user.permalink).first.permalink.should == @user.nickname.parameterize
-    end
-
-    it "should set a gravatar_url" do
-      User.by_permalink(@user.permalink).first.gravatar_url.should == "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(@user.email.downcase.strip)}.jpg?size="
+      it "should set a gravatar_url" do
+        User.by_permalink(@user.permalink).first.gravatar_url.should == "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(@user.email.downcase.strip)}.jpg?size="
+      end
     end
   end
 
