0ff98e7fffe21e48ca3f4123b40434f68ff86a2a
chatgris <chatgri@gmail.com>
2010-08-15 15:56:36 +0200

    carrierwave spec config

--- config/initializers/carrierwave_test.rb
+++
@@ -1,5 +0,0 @@
-if Rails.env.test? or Rails.env.cucumber?
-  CarrierWave.configure do |config|
-    config.enable_processing = false
-  end
-end
---
+++ spec/support/carrierwave.rb
@@ -0,0 +1,5 @@
+require 'carrierwave/test/matchers'
+
+CarrierWave.configure do |config|
+  config.enable_processing = false
+end
