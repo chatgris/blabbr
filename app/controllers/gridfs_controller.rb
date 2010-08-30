152805e2048836050f67c1e1184ce03d7cfe947b
chatgris <chatgri@gmail.com>
2010-08-30 20:24:40 +0200

    Add cache control on gridfs

--- app/controllers/gridfs_controller.rb
+++ app/controllers/gridfs_controller.rb
@@ -7,6 +7,7 @@ class GridfsController < ActionController::Metal
       gridfs_file = Mongo::GridFileSystem.new(Mongoid.database).open(gridfs_path, 'r')
       self.response_body = gridfs_file.read
       self.content_type = gridfs_file.content_type
+      self.headers['Cache-Control'] = 'public, max-age=2592000'
     rescue
       self.status = :file_not_found
       self.content_type = 'text/plain'
--- lib/textilizer.rb
+++ lib/textilizer.rb
@@ -9,7 +9,7 @@ class Textilizer
 
   def smilirize(text)
     @smilies.each do |smiley|
-      text.gsub!(/:#{smiley.code}:/, "<img src=\"#{smiley.image.url}\" alt=\"#{smiley.code}\" />")
+      text.gsub!(/:#{smiley.code}:/, "<img src=\"#{smiley.image.url}?#{smiley.updated_at.to_i.to_s}\" alt=\"#{smiley.code}\" />")
     end
     text
   end
