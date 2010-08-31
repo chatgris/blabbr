3ac14135d65a83c854e703d2be0437133946cfe4
chatgris <chatgri@gmail.com>
2010-08-31 15:56:24 +0200

    Add blink effect on page title when a new post is added

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,50 @@
+var titleHolder = document.title;
+
 jQuery(function($){//on document ready
   //autocomplete
   $('input.autocomplete').each(function(){
     var $input = $(this);
     $input.autocomplete($input.attr('data-autocomplete-url'));
   });
+
+  $('html').mouseover(function() {
+    gainedFocus();
+  });
+
 });
+
 function updatePosts(url){
   $.get(url,function(data){
       if (data) {
         $(data).hide().appendTo("#posts").show('slow');
+        lostFocus();
+        blinkTitle(1);
         document.getElementById('player').play();
       }
   },'js');
 }
+
+function blinkTitle(state) {
+  if (windowIsActive != true) {
+    if (state == 1) {
+      document.title = "[new!] - " + titleHolder;
+      state = 0;
+    } else {
+      document.title = "" + titleHolder;
+      state = 1;
+    }
+
+    setTimeout("blinkTitle(" + state + ")", 1600);
+  } else {
+    document.title = titleHolder;
+  }
+}
+
+function lostFocus() {
+  windowIsActive = false;
+}
+
+function gainedFocus() {
+  windowIsActive = true;
+}
+
