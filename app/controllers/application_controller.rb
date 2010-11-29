ff7e0a332e52e7190d52d1592fe69f27f5ef4036
chatgris <julien.boyer@af83.com>
2010-11-29 21:36:38 +0100

    redirect to https if ENV['SSL'] is defined

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   layout :layout_by_resource
   protect_from_forgery
   helper_method :creator?
+  before_filter :redirect_to_https
   after_filter :flash_to_headers
 
   protected
@@ -24,6 +25,10 @@ class ApplicationController < ActionController::Base
 
   def flash_to_headers
     flash.discard if request.xhr?
+  end
+
+  def redirect_to_https
+    redirect_to :protocol => "https://" unless (request.ssl? || ENV['SSL'].nil?)
   end
 
 end
