35f375cb712497ec5ddd43f365764904e148ffba
chatgris <jboyer@af83.com>
2011-04-24 21:23:16 +0200

    Devise upgrade notice

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -43,7 +43,6 @@ Devise.setup do |config|
   # from others authentication tools as :clearance_sha1, :authlogic_sha512 (then
   # you should set stretches above to 20 for default behavior) and :restful_authentication_sha1
   # (then you should set stretches to 10, and copy REST_AUTH_SITE_KEY to pepper)
-  config.encryptor = :bcrypt
 
   # Setup a pepper to generate the encrypted password.
   config.pepper = "fd430ccce49714e4251cd745bbfc1be4ae831156decef4ea29ae70fa99fc0db53e6c69c4d85d5fe16ebc1f75f725247b8443c220eb5aef45e5614a6b58a255b9"
