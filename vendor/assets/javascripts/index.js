95fab63d6c1d86df87e3ee06f697e6ac44dff870
chatgris <jboyer@af83.com>
2011-06-07 21:05:40 +0200

    remove dead code

--- vendor/assets/javascripts/index.js
+++
@@ -1,7 +0,0 @@
-//= require ../../../lib/assets/javascripts/jquery-1.6.1
-//= require ./jquery.autocomplete
-//= require ./jquery.sexypost
-//= require ./pusher
-//= require ./sammy
-//= require ./sammy.nested_params
-//= require ./ICanHaz
