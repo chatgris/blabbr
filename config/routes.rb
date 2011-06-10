16d0246f7c3a0f84a86860bd23571f866d9da9ad
chatgris <jboyer@af83.com>
2011-06-10 15:09:49 +0200

    Only responds to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
+class JsonConstraint
+  def matches?(request)
+    request.format.json?
+  end
+end
 Blabbr::Application.routes.draw do
 
   devise_for :users do
@@ -5,29 +10,33 @@ Blabbr::Application.routes.draw do
   end
 
   authenticate :user do
-    match "/uploads/*path" => "gridfs#serve"
+    constraints JsonConstraint.new do
 
-    resources :users, :only => [:show, :create, :update] do
-      collection do
-        get :autocomplete
-        get :current
+      resources :users, :only => [:show, :create, :update], :constraints => JsonConstraint.new do
+        collection do
+          get :autocomplete
+          get :current
+        end
       end
+
+      resources :smilies, :as => "smileys", :except => [:show]
+
+      match '/topics/:id/page/:page' => 'topics#show', :as => "page_topic"
+      match '/topics/page/:page' => 'topics#index', :as => "page_topics"
+
+      resources :topics do
+        resources :posts
+        put :add_member, :on => :member
+        put :rm_member, :on => :member
+      end
+
+      match 'dashboard' => 'users#edit', :as => :dashboard
+
+      root :to => 'topics#index'
     end
-
-    resources :smilies, :as => "smileys", :except => [:show]
-
-    match '/topics/:id/page/:page' => 'topics#show', :as => "page_topic"
-    match '/topics/page/:page' => 'topics#index', :as => "page_topics"
-
-    resources :topics do
-      resources :posts
-      put :add_member, :on => :member
-      put :rm_member, :on => :member
-    end
-
-    match 'dashboard' => 'users#edit', :as => :dashboard
-
-    root :to => 'topics#index'
+    match "/uploads/*path" => "gridfs#serve"
+    root :to => 'home#index'
+    match '*a', :to => 'home#index'
   end
 
 end
