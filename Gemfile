788b552d10bfdad8eb76714408727bbb41b4bcd2
chatgris <jboyer@af83.com>
2012-04-08 19:03:01 +0200

    Plug blabbr_core.
    Signed-off-by: chatgris <jboyer@af83.com>

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@
 source :rubygems
 
 gem 'sinatra', git: 'git://github.com/sinatra/sinatra.git'
+gem 'blabbr_core', git: 'git://github.com/chatgris/blabbr_core.git'
 gem 'warden'
 gem 'thin'
--- blabbr.rb
+++ blabbr.rb
@@ -1,5 +1,11 @@
 # encoding: utf-8
 require 'sinatra/base'
+require 'blabbr_core'
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db('blabbr_dev')
+  config.identity_map_enabled = true
+end
 
 # App
 Dir[File.dirname(__FILE__) + "/app/**/*.rb"].each {|file| require file }
